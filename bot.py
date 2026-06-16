a3d37f4cb7b4afbc7bbcd787d1edec26e2130292
Saurav Sharma <saurav818079@gmail.com>
2026-06-16 23:18:50 +0530

    fix event loop

--- bot.py
+++ bot.py
@@ -1,46 +1,11 @@
-import os
-from dotenv import load_dotenv
-
-from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters
-)
-
-load_dotenv()
-
-TOKEN = os.getenv("BOT_TOKEN")
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 Bot Started")
-
-
-async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Link received ✅"
-    )
+from telegram.ext import Application
+from config import TOKEN
 
 
 def main():
-
-    print("BOT STARTED")
-
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(
-        CommandHandler("start", start)
-    )
-
-    app.add_handler(
-        MessageHandler(
-            filters.TEXT,
-            message
-        )
-    )
+    print("BOT STARTED")
 
     app.run_polling()
 
