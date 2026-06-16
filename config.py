5180517243ab7cbc57079c740ce27e258ba30082
Saurav Sharma <saurav818079@gmail.com>
2026-06-16 13:58:07 +0530

    fix telegram package

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8632272810:AAE5ABa0amwLYIbiUKKFSGgT7WZe2RytTB8")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN missing")
